3aae893ed5787733615ade6b390093bc0da8a5b6
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2024-02-05 16:40:52 -0500

    proj setup

---
+++ Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Raylib_cs;
+using System.Numerics;
+
+namespace SlumWarriors
+{
+    internal class Engine
+    {
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SlumWarriors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
